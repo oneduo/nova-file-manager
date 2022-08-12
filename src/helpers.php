18b1716a623a11a6ae87c9531a59fd10b99dd60d
Mikaël Popowicz <mikael.popowicz@gmail.com>
2022-08-12 20:07:14 +0200

    Add str helper for laravel 8.

---
+++ src/helpers.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Str;
+
+if (! function_exists('str')) {
+    /**
+     * Get a new stringable object from the given string.
+     *
+     * @param  string|null  $string
+     * @return \Illuminate\Support\Stringable|mixed
+     */
+    function str($string = null)
+    {
+        if (func_num_args() === 0) {
+            return new class
+            {
+                public function __call($method, $parameters)
+                {
+                    return Str::$method(...$parameters);
+                }
+
+                public function __toString()
+                {
+                    return '';
+                }
+            };
+        }
+
+        return Str::of($string);
+    }
+}
