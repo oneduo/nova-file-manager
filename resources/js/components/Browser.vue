7823a0e832845799fb01782cf8145adf3f726a2e
Mikaël Popowicz <mikael.popowicz@gmail.com>
2022-08-12 20:14:26 +0200

    ♻️ wip composition.

--- resources/js/components/Browser.vue
+++ resources/js/components/Browser.vue
@@ -2,33 +2,33 @@
   <div class="flex flex-1 items-stretch overflow-hidden rounded-md w-full min-h-[30vh]">
     <main class="relative flex-1 overflow-y-auto bg-gray-50 dark:bg-gray-800">
       <div class="w-full px-4 space-y-4 mb-4">
-        <Toolbar />
+        <Toolbar/>
 
         <div
-          class="w-full h-80 flex justify-center items-center"
           v-if="isFetchingData"
+          class="w-full h-80 flex justify-center items-center"
         >
-          <Spinner class="w-16 h-16" />
+          <Spinner class="w-16 h-16"/>
         </div>
 
         <template v-else>
           <BrowserContent
-            :view="view"
-            :files="files"
             :directories="directories"
+            :files="files"
             :filled="filled"
+            :view="view"
           />
         </template>
       </div>
 
       <Pagination
         v-if="!isFetchingData && pagination && pagination.total > 0"
-        :from="pagination.from"
-        :to="pagination.to"
-        :links="pagination.links"
-        :total="pagination.total"
         :current-page="pagination.current_page"
+        :from="pagination.from"
         :last-page="pagination.last_page"
+        :links="pagination.links"
+        :to="pagination.to"
+        :total="pagination.total"
         class="mt-auto"
       />
     </main>
@@ -36,57 +36,29 @@
   <div id="modals"></div>
 </template>
 
-<script>
+<script setup>
+import { computed, onBeforeUnmount, onMounted } from 'vue'
+import { useStore } from 'vuex'
 import Toolbar from '@/components/Toolbar'
 import Pagination from '@/components/Pagination'
 import Spinner from '@/components/Elements/Spinner'
-import { mapActions, mapMutations, mapState } from 'vuex'
 import BrowserContent from '@/components/BrowserContent'
 
-export default {
-  components: {
-    BrowserContent,
-    Toolbar,
-    Pagination,
-    Spinner,
-  },
+const store = useStore()
+const files = computed(() => store.state['nova-file-manager'].files)
+const directories = computed(() => store.state['nova-file-manager'].directories)
+const filled = computed(() => files.value?.length || directories.value?.length)
+const pagination = computed(() => store.state['nova-file-manager'].pagination)
+const view = computed(() => store.state['nova-file-manager'].view)
+const isFetchingData = computed(() => store.state['nova-file-manager'].isFetchingData)
 
-  mounted() {
-    this.init()
-    this.getDisks()
-    this.getData()
-  },
+onMounted(() => {
+  store.commit('nova-file-manager/init')
+  store.dispatch('nova-file-manager/getDisks')
+  store.dispatch('nova-file-manager/getData')
+})
 
-  beforeUnmount() {
-    this.destroy()
-  },
-  computed: {
-    ...mapState('nova-file-manager', [
-      'disk',
-      'disks',
-      'path',
-      'files',
-      'directories',
-      'breadcrumbs',
-      'pagination',
-      'page',
-      'perPage',
-      'perPageOptions',
-      'selectedFile',
-      'view',
-      'isFetchingData',
-      'isFetchingDisks',
-      'isPreviewOpen',
-      'isFieldMode',
-    ]),
-    filled() {
-      return this.files?.length || this.directories?.length
-    },
-  },
-
-  methods: {
-    ...mapMutations('nova-file-manager', ['init', 'destroy', 'setSelectedFile']),
-    ...mapActions('nova-file-manager', ['setPath', 'getData', 'getDisks']),
-  },
-}
+onBeforeUnmount(() => {
+  store.commit('nova-file-manager/destroy')
+})
 </script>
