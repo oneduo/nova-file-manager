f3059fd8725182a2c252b09671f07929ca0c2028
crezra <crezra@users.noreply.github.com>
2022-08-21 17:08:32 +0000

    deploy: b0768a62e905f8cf756fc32703d882766a7409fd

--- assets/introduction.md.a0bf34e2.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as i,o as e,a as s}from"./app.b0bccede.js";const h=JSON.parse('{"title":"Nova File Manager","description":"","frontmatter":"layout:home","headers":[],"relativePath":"introduction.md"}'),t={name:"introduction.md"},l=s("",5),o=[l];function n(r,d,c,g,m,p){return e(),i("div",null,o)}var v=a(t,[["render",n]]);export{h as __pageData,v as default};
--- assets/requirements.md.680595d9.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as r,a}from"./app.b0bccede.js";const m=JSON.parse('{"title":"Requirements","description":"","frontmatter":{},"headers":[],"relativePath":"requirements.md"}'),o={name:"requirements.md"},s=a("",4),n=[s];function i(d,c,h,_,u,l){return r(),t("div",null,n)}var f=e(o,[["render",i]]);export{m as __pageData,f as default};
