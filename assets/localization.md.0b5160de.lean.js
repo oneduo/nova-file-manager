9023d8ec51b8d7850ff24270506809248de96292
crezra <crezra@users.noreply.github.com>
2022-09-02 13:36:22 +0000

    deploy: 951ed290f50d4ef51369f7c2093a5efa7ad03486

---
+++ assets/localization.md.0b5160de.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o,a as n}from"./app.57be7019.js";const A=JSON.parse('{"title":"Localization","description":"","frontmatter":{},"headers":[],"relativePath":"localization.md"}'),l={name:"localization.md"},p=n("",3),e=[p];function t(D,r,F,c,y,C){return o(),a("div",null,e)}var u=s(l,[["render",t]]);export{A as __pageData,u as default};
