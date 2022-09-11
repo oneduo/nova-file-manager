6c3eafc0d6e356c9b0e2ed246cad4dd7f26f81c4
Charaf Rezrazi <charaf@big-boss-studio.com>
2022-09-11 23:40:00 +0200

    feat: added `toEntity` transformer

---
+++ resources/js/transformers/index.js
@@ -0,0 +1,20 @@
+import Entity from '@/types/Entity'
+
+/**
+ * Transform a native File to an Entity
+ *
+ * @param {File} file
+ */
+const toEntity = file => {
+  return new Entity(
+    file.name,
+    file.name,
+    file.name,
+    null,
+    null,
+    null,
+    URL.createObjectURL(file),
+    null,
+    file.type.split('/')[0]
+  )
+}
