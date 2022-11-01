fe424e876cb2ed2352154b5eb998f05478909906
Mikaël Popowicz <mikael.popowicz@gmail.com>
2022-11-01 14:50:59 +0100

    fix(translations): fix translation merge.
    Ensures current translations are an array before merging with english translations.

--- src/ToolServiceProvider.php
+++ src/ToolServiceProvider.php
@@ -32,9 +32,15 @@ class ToolServiceProvider extends ServiceProvider
         $this->translations();
 
         Nova::serving(function () {
-            Nova::translations(
-                trans('nova-file-manager::ui', [], 'en')
-            );
+            $translations = trans('nova-file-manager::ui');
+
+            if (!is_array($translations)) {
+                $translations = [];
+            }
+
+            $translations = array_merge(trans('nova-file-manager::ui', [], 'en'), $translations);
+
+            Nova::translations($translations);
 
             Nova::style('nova-file-manager', __DIR__.'/../dist/css/tool.css');
 
