50e3308ae5a70c565dc8ac164e9e5bf2de5fc6f3
Angelos Michalopoulos <a-mi@otenet.gr>
2023-01-25 20:27:11 +0200

    Fix preview on detail field

--- resources/js/hooks/usePermissions.js
+++ resources/js/hooks/usePermissions.js
@@ -4,14 +4,14 @@ import { computed } from 'vue'
 export function usePermissions() {
   const store = useStore()
 
-  const showCreateFolder = computed(() => store.permissions.folder.create)
-  const showRenameFolder = computed(() => store.permissions.folder.rename)
-  const showDeleteFolder = computed(() => store.permissions.folder.delete)
-  const showUploadFile = computed(() => store.permissions.file.upload)
-  const showRenameFile = computed(() => store.permissions.file.rename)
-  const showDeleteFile = computed(() => store.permissions.file.delete)
-  const showCropImage = computed(() => store.permissions.file.edit)
-  const showUnzipFile = computed(() => store.permissions.file.unzip)
+  const showCreateFolder = computed(() => store.permissions?.folder?.create)
+  const showRenameFolder = computed(() => store.permissions?.folder?.rename)
+  const showDeleteFolder = computed(() => store.permissions?.folder?.delete)
+  const showUploadFile = computed(() => store.permissions?.file?.upload)
+  const showRenameFile = computed(() => store.permissions?.file?.rename)
+  const showDeleteFile = computed(() => store.permissions?.file?.delete)
+  const showCropImage = computed(() => store.permissions?.file?.edit)
+  const showUnzipFile = computed(() => store.permissions?.file?.unzip)
 
   return {
     showCreateFolder,
