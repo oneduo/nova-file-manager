ecd5c903746bf77b10af93235cdd00f9c95e80ff
Angelos Michalopoulos <a-mi@otenet.gr>
2023-01-25 19:47:11 +0200

    Fix preview bug on List mode

--- resources/js/components/List.vue
+++ resources/js/components/List.vue
@@ -2,13 +2,13 @@
   <table class="min-w-full divide-y divide-gray-300 dark:divide-gray-600/50">
     <tbody class="divide-y divide-gray-200 dark:divide-gray-900/20">
       <tr v-if="folders?.length" class="border-t border-gray-200 dark:border-gray-700/50">
-        <th class="py-2 text-left text-xs font-semibold text-gray-500" colspan="2" scope="colgroup">
+        <th class="py-2 text-xs font-semibold text-left text-gray-500" colspan="2" scope="colgroup">
           {{ __('Folders') }}
         </th>
       </tr>
       <template v-for="folder in folders" :key="folder.id">
         <tr class="cursor-pointer">
-          <td class="whitespace-nowrap py-4 text-sm" @click="setPath(folder.path)">
+          <td class="py-4 text-sm whitespace-nowrap" @click="setPath(folder.path)">
             <div class="flex items-center text-gray-500 dark:text-gray-300 hover:text-blue-500">
               <FolderIcon class="w-5 h-5" />
               <div class="ml-4">
@@ -19,25 +19,35 @@
             </div>
           </td>
           <td
-            class="relative whitespace-nowrap py-4 text-right text-xs font-medium"
+            class="relative py-4 text-xs font-medium text-right whitespace-nowrap"
             v-if="showRenameFolder || showDeleteFolder"
           >
             <div class="inline-flex items-center">
-              <div class="relative flex-1 flex items-center justify-end">
+              <div class="relative flex items-center justify-end flex-1">
                 <Menu as="div" class="relative inline-block text-left">
                   <MenuButton
-                    class="flex items-center text-gray-500 hover:text-blue-500 hover:bg-gray-50 dark:hover:bg-gray-600/50 rounded-full focus:outline-none mr-2 p-0.5"
+                    class="
+                      flex
+                      items-center
+                      text-gray-500
+                      hover:text-blue-500 hover:bg-gray-50
+                      dark:hover:bg-gray-600/50
+                      rounded-full
+                      focus:outline-none
+                      mr-2
+                      p-0.5
+                    "
                   >
-                    <EllipsisHorizontalIcon class="h-3 w-3" />
+                    <EllipsisHorizontalIcon class="w-3 h-3" />
                   </MenuButton>
 
                   <MenuItems
-                    class="z-50 origin-top-right absolute right-0 mt-2 w-36 select-none overflow-hidden bg-white dark:bg-gray-900 rounded-md border border-gray-200 dark:border-gray-700 px-1"
+                    class="absolute right-0 z-50 px-1 mt-2 overflow-hidden origin-top-right bg-white border border-gray-200 rounded-md select-none  w-36 dark:bg-gray-900 dark:border-gray-700"
                   >
                     <div class="py-1">
                       <MenuItem v-if="showRenameFolder">
                         <button
-                          class="hover:bg-gray-50 dark:hover:bg-gray-800 block w-full text-left cursor-pointer py-2 px-3 focus:outline-none focus:ring rounded truncate whitespace-nowrap text-gray-500 active:text-gray-600 dark:text-gray-500 dark:hover:text-gray-400 dark:active:text-gray-600"
+                          class="block w-full px-3 py-2 text-left text-gray-500 truncate rounded cursor-pointer  hover:bg-gray-50 dark:hover:bg-gray-800 focus:outline-none focus:ring whitespace-nowrap active:text-gray-600 dark:text-gray-500 dark:hover:text-gray-400 dark:active:text-gray-600"
                           @click="openModal(`rename-folder-${folder.id}`)"
                         >
                           {{ __('Rename') }}
@@ -45,7 +55,7 @@
                       </MenuItem>
                       <MenuItem v-if="showDeleteFolder">
                         <button
-                          class="hover:bg-red-50 dark:hover:bg-red-600/20 block w-full text-left cursor-pointer py-2 px-3 focus:outline-none focus:ring rounded truncate whitespace-nowrap text-red-500 dark:text-red-500 dark:hover:text-red-700"
+                          class="block w-full px-3 py-2 text-left text-red-500 truncate rounded cursor-pointer  hover:bg-red-50 dark:hover:bg-red-600/20 focus:outline-none focus:ring whitespace-nowrap dark:text-red-500 dark:hover:text-red-700"
                           @click="openModal(`delete-folder-${folder.id}`)"
                         >
                           {{ __('Delete') }}
@@ -72,13 +82,13 @@
         />
       </template>
       <tr v-if="files?.length" class="border-t border-gray-200 dark:border-gray-700">
-        <th class="py-2 text-left text-xs font-semibold text-gray-500" colspan="2" scope="colgroup">
+        <th class="py-2 text-xs font-semibold text-left text-gray-500" colspan="2" scope="colgroup">
           {{ __('Files') }}
         </th>
       </tr>
       <template v-for="file in files" :key="file.id">
         <tr class="cursor-pointer" @click="toggleSelection(file)" @dblclick="openPreview(file)">
-          <td class="whitespace-nowrap py-4 text-sm w-full" colspan="2">
+          <td class="w-full py-4 text-sm whitespace-nowrap" colspan="2">
             <div
               :class="[
                 isSelected(file)
@@ -95,7 +105,7 @@
               </div>
             </div>
           </td>
-          <PreviewModal :file="entity(file)" />
+          <PreviewModal :file="entity(file)" v-if="!!preview && preview?.id === entity(file)?.id" />
         </tr>
       </template>
     </tbody>
@@ -121,6 +131,7 @@ const { showRenameFolder, showDeleteFolder } = usePermissions()
 const files = computed(() => store.files)
 const folders = computed(() => store.folders)
 const isSelected = computed(() => store.isSelected)
+const preview = computed(() => store.preview)
 
 // ACTIONS
 const onFolderRename = (id, from, to) => store.renameFile({ id, from, to })
