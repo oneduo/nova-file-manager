48b128142762ae727ec2f4b1fb5a22f3a6a63b7f
Charaf Rezrazi <2086576+Rezrazi@users.noreply.github.com>
2023-01-28 19:20:24 +0100

    refactor: removed lodash stuff

--- resources/js/fields/IndexField.vue
+++ resources/js/fields/IndexField.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import { DocumentIcon } from '@heroicons/vue/24/outline'
 import { Entity, NovaField } from '__types__'
-import isNil from 'lodash/isNil'
-import take from 'lodash/take'
 import { computed, onMounted } from 'vue'
 import useBrowserStore from '@/stores/browser'
 
@@ -16,27 +14,28 @@ const dark = computed(() => store.dark)
 
 onMounted(() => store.syncDarkMode())
 
-const filled = (value?: Entity | Entity[] | null) => !isNil(value)
+const filled = (value?: Entity | Entity[] | null) => !!value
 
 const usesCustomizedDisplay = computed(() => props.field.usesCustomizedDisplay && filled(props.field.displayedAs))
 const shouldDisplayAsHtml = computed(() => props.field.asHtml)
-
 const values = computed(() => props.field.value || [])
-const thumbnails = computed(() => take(values.value, 3))
+const thumbnails = computed(() => values.value.slice(0, 3))
 const rest = computed(() => Math.max(values.value.length - 3, 0))
-
 const fieldHasValue = computed(() => !!props.field.value?.length)
+
 const fieldValue = computed(() => {
   if (!usesCustomizedDisplay.value && !fieldHasValue.value) {
     return null
   }
 
-  return usesCustomizedDisplay.value ? String(props.field.displayedAs) : props.field.value
+  return usesCustomizedDisplay.value ? props.field.displayedAs.toString() : props.field.value
 })
+
+const elevations = ['z-10', 'z-20', 'z-30']
 </script>
 
 <template>
-  <div :class="`text-${field.textAlign} nova-file-manager`">
+  <div class="nova-file-manager">
     <div :class="{ dark }">
       <template v-if="usesCustomizedDisplay">
         <span v-if="fieldValue && !shouldDisplayAsHtml" class="text-90 whitespace-nowrap">
@@ -47,18 +46,19 @@ const fieldValue = computed(() => {
       <template v-else>
         <div v-if="fieldHasValue">
           <div class="isolate flex -space-x-2">
-            <template v-for="(file, index) in thumbnails" :key="file.id">
+            <template v-for="(file, index) in thumbnails" :key="`thumbnail-${file.id}`">
               <img
                 v-if="file.type === 'image'"
                 :src="file.url"
                 class="relative inline-block h-10 w-10 rounded-xl ring-gray-200 dark:ring-gray-700 ring-1 shadow object-cover"
-                :class="`z-${index + 1}0`"
+                :class="elevations[index]"
+                :alt="file.name"
               />
 
               <template v-else-if="file.type === 'video'">
                 <video
                   class="relative inline-block h-10 w-10 rounded-xl ring-gray-200 dark:ring-gray-700 ring-1 shadow object-cover"
-                  :class="`z-${index + 1}0`"
+                  :class="elevations[index]"
                 >
                   <source :src="file.url" />
                   {{ __("Sorry, your browser doesn't support embedded videos.") }}
@@ -68,7 +68,7 @@ const fieldValue = computed(() => {
               <div
                 v-else
                 class="relative inline-block flex items-center justify-center h-10 w-10 rounded-xl ring-gray-200 dark:ring-gray-700 ring-1 shadow bg-gray-50 dark:bg-gray-900"
-                :class="`z-${index + 1}0`"
+                :class="elevations[index]"
               >
                 <DocumentIcon class="h-6 w-6" />
               </div>
