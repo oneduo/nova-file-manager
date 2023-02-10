b0ce471f65aef1d63cb7ab79bf842948cee845bf
mikaelpopowicz <mikaelpopowicz@users.noreply.github.com>
2023-02-10 09:42:51 +0000

    deploy: d7f3bbc920ba803cc0d597fbfb546344386c889b

--- assets/localization.md.f4c9080e.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as a,o,a as n}from"./app.8661ceea.js";const A=JSON.parse('{"title":"Localization","description":"","frontmatter":{},"headers":[],"relativePath":"localization.md"}'),l={name:"localization.md"},p=n("",4),e=[p];function t(D,r,F,c,y,C){return o(),a("div",null,e)}var u=s(l,[["render",t]]);export{A as __pageData,u as default};
--- assets/validation.md.7f6e701f.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as a,o as n,a as l}from"./app.8661ceea.js";const i=JSON.parse('{"title":"Validation","description":"","frontmatter":{},"headers":[],"relativePath":"validation.md"}'),p={name:"validation.md"},o=l("",5),e=[o];function t(c,r,D,y,F,C){return n(),a("div",null,e)}var d=s(p,[["render",t]]);export{i as __pageData,d as default};
